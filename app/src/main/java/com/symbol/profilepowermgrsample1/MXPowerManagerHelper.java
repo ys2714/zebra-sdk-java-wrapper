1d55140ea3af5b4958b930c31a07611c8f9a8e6f
Sun <YS2714@zebra.com>
2025-10-17 19:01:48 +0900

    init

---
+++ app/src/main/java/com/symbol/profilepowermgrsample1/MXPowerManagerHelper.java
@@ -0,0 +1,261 @@
+package com.symbol.profilepowermgrsample1;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Xml;
+
+import com.symbol.emdk.*;
+import com.symbol.emdk.EMDKManager.EMDKListener;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.StringReader;
+
+public class MXPowerManagerHelper implements EMDKListener {
+
+    public interface EventListener {
+        void onEMDKSessionOpened();
+        void onEMDKSessionClosed();
+        void onEMDKError(XMLErrorInfo errorInfo);
+    }
+    // Initial Value of the Power Manager options to be executed in the
+    // onOpened() method when the EMDK is ready. Default Value set in the wizard
+    // is 0.
+    // 0 -> Do Nothing
+    // 1 -> Sleep Mode
+    // 4 -> Reboot
+    // 5 -> Enterprise Reset
+    // 6 -> Factory Reset
+    // 7 -> Full Device Wipe
+    // 8 -> OS Update
+
+    public enum Options {
+        CREATE_PROFILE(-1),
+        DO_NOTHING(0),
+        SLEEP_MODE(1),
+        REBOOT(4),
+        ENTERPRISE_RESET(5),
+        FACTORY_RESET(6),
+        FULL_DEVICE_WIPE(7),
+        OS_UPDATE(8);
+
+        private final int value;
+
+        Options(int value) {
+            this.value = value;
+        }
+    }
+
+    public static class XMLErrorInfo {
+        // Contains the parm-error name (sub-feature that has error)
+        String errorName = "";
+        // Contains the characteristic-error type (Root feature that has error)
+        String errorType = "";
+        // contains the error description for parm or characteristic error.
+        String errorDescription = "";
+
+        public String buildFailureMessage() {
+            String failureMessage = "";
+            if (!TextUtils.isEmpty(errorName) && !TextUtils.isEmpty(errorType))
+                failureMessage = errorName + " :" + "\n" + errorType + " :" + "\n"
+                        + errorDescription;
+            else if (!TextUtils.isEmpty(errorName))
+                failureMessage = errorName + " :" + "\n" + errorDescription;
+            else
+                failureMessage = errorType + " :" + "\n" + errorDescription;
+            return failureMessage;
+        }
+    }
+
+    // contains status of the profile operation
+    private String profileName = null;
+    private EventListener listener = null;
+    private ProfileManager profileManager = null;
+    private EMDKManager emdkManager = null;
+
+    public MXPowerManagerHelper(EventListener listener) {
+        this.listener = listener;
+    }
+
+    public void setupEMDK(Context ctx, String profileName) {
+        this.profileName = profileName;
+
+        //The EMDKManager object will be created and returned in the callback.
+        EMDKResults results = EMDKManager.getEMDKManager(ctx.getApplicationContext(), this);
+
+        //Check the return status of getEMDKManager
+        if (results.statusCode == EMDKResults.STATUS_CODE.SUCCESS) {
+            // EMDKManager object creation success
+        } else {
+            // EMDKManager object creation failed
+        }
+    }
+
+    public void releaseEMDK() {
+        emdkManager.release();
+        emdkManager = null;
+        profileManager = null;
+    }
+
+    @Override
+    public void onOpened(EMDKManager emdkManager) {
+        // This callback will be issued when the EMDK is ready to use.
+        // Get the ProfileManager object to process the profiles
+        profileManager = (ProfileManager) emdkManager
+                .getInstance(EMDKManager.FEATURE_TYPE.PROFILE);
+
+        callFeature(Options.CREATE_PROFILE, null);
+
+        listener.onEMDKSessionOpened();
+    }
+
+    @Override
+    public void onClosed() {
+        listener.onEMDKSessionClosed();
+    }
+
+    // Method to handle EMDKResult by extracting response and parsing it
+    private void handleEMDKResult(EMDKResults results) {
+        // Get XML response as a String
+        String statusXMLResponse = results.getStatusString();
+        XMLErrorInfo errorInfo = null;
+        try {
+            // Create instance of XML Pull Parser to parse the response
+            XmlPullParser parser = Xml.newPullParser();
+            // Provide the string response to the String Reader that reads
+            // for the parser
+            parser.setInput(new StringReader(statusXMLResponse));
+            // Call method to parse the response
+            errorInfo = parseXML(parser);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+        if (errorInfo != null) {
+            listener.onEMDKError(errorInfo);
+        }
+    }
+
+    // Method to parse the XML response using XML Pull Parser
+    private XMLErrorInfo parseXML(XmlPullParser myParser) {
+        XMLErrorInfo errorInfo = null;
+        int event;
+        try {
+            event = myParser.getEventType();
+            while (event != XmlPullParser.END_DOCUMENT) {
+                String name = myParser.getName();
+                switch (event) {
+                    case XmlPullParser.START_TAG:
+                        // Get Status, error name and description in case of
+                        // parm-error
+                        if (name.equals("parm-error")) {
+                            errorInfo = new XMLErrorInfo();
+                            errorInfo.errorName = myParser.getAttributeValue(null, "name");
+                            errorInfo.errorDescription = myParser.getAttributeValue(null,
+                                    "desc");
+
+                            // Get Status, error type and description in case of
+                            // parm-error
+                        } else if (name.equals("characteristic-error")) {
+                            errorInfo = new XMLErrorInfo();
+                            errorInfo.errorType = myParser.getAttributeValue(null, "type");
+                            errorInfo.errorDescription = myParser.getAttributeValue(null,
+                                    "desc");
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                event = myParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            errorInfo.errorName = "unknown error";
+            errorInfo.errorDescription = e.getLocalizedMessage();
+        }
+        return errorInfo;
+    }
+
+    // Method that applies the modified settings to the EMDK Profile based on
+    // user selected options of Power Manager feature.
+    public void callFeature(Options option, String zipFilePath) {
+        int value = option.value;
+
+        if (profileManager == null) {
+            profileManager = (ProfileManager) emdkManager
+                    .getInstance(EMDKManager.FEATURE_TYPE.PROFILE);
+        }
+
+        // Prepare XML to modify the existing profile
+        String[] modifyData = new String[1];
+        if (option == Options.CREATE_PROFILE) {
+            // Call processPrfoile with profile name and SET flag to create the
+            // profile. The modifyData can be null.
+        } else if (option == Options.OS_UPDATE) {
+            // String that gets the path of the OS Update Package from Edit Text
+            // If the OS Package path entered by user is empty then display
+            // a Toast
+            if (TextUtils.isEmpty(zipFilePath)) {
+                XMLErrorInfo errorInfo = new XMLErrorInfo();
+                errorInfo.errorType = "File Path";
+                errorInfo.errorName = "Incorrect Path";
+                errorInfo.errorDescription = "Incorrect File Path...";
+                listener.onEMDKError(errorInfo);
+
+                return;
+            }
+
+            // Modified XML input for OS Update feature that contains path
+            // to the update package
+            modifyData[0] = "&lt;?xml version=\"1.0\" encoding=\"utf-8\"?&gt;"
+                    + "&lt;characteristic type=\"Profile\"&gt;"
+                    + "&lt;parm name=\"ProfileName\" value=\"" + profileName + "\"/&gt;"
+                    + "&lt;characteristic type=\"PowerMgr\"&gt;"
+                    + "&lt;parm name=\"ResetAction\" value=\"" + value + "\"/&gt;"
+                    + "&lt;characteristic type=\"file-details\"&gt;"
+                    + "&lt;parm name=\"ZipFile\" value=\"" + zipFilePath + "\"/&gt;"
+                    + "&lt;/characteristic&gt;" + "&lt;/characteristic&gt;"
+                    + "&lt;/characteristic&gt;";
+
+
+        } else {
+            // Modified XML input for Sleep and Reboot feature based on user
+            // selected options of radio button
+            // value = 1 -> Sleep Mode
+            // value = 4 -> Rebbot
+            modifyData[0] = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+                    + "<characteristic type=\"Profile\">"
+                    + "<parm name=\"ProfileName\" value=\"" + profileName + "\"/>"
+                    + "<characteristic type=\"PowerMgr\">"
+                    + "<parm name=\"ResetAction\" value=\"" + value + "\"/>"
+                    + "</characteristic>" + "</characteristic>";
+        }
+
+        // Call process profile to modify the profile of specified profile
+        // name
+        EMDKResults results = profileManager.processProfile(profileName,
+                ProfileManager.PROFILE_FLAG.SET, modifyData);
+
+        if (option == Options.CREATE_PROFILE) {
+            return;
+        }
+
+        if (results.statusCode == EMDKResults.STATUS_CODE.CHECK_XML) {
+            // Method call to handle EMDKResult
+            handleEMDKResult(results);
+        } else if (results.statusCode == EMDKResults.STATUS_CODE.FAILURE) {
+            XMLErrorInfo errorInfo = new XMLErrorInfo();
+            errorInfo.errorType = "set profile";
+            errorInfo.errorName = "set profile error";
+            errorInfo.errorDescription = results.statusCode.toString();
+            listener.onEMDKError(errorInfo);
+        } else {
+            XMLErrorInfo errorInfo = new XMLErrorInfo();
+            errorInfo.errorType = "set profile";
+            errorInfo.errorName = "unknown error";
+            errorInfo.errorDescription = results.statusCode.toString();
+            listener.onEMDKError(errorInfo);
+        }
+    }
+}
---
+++ app/src/main/java/com/symbol/profilepowermgrsample1/MainActivity.java
@@ -0,0 +1,118 @@
+package com.symbol.profilepowermgrsample1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.symbol.profilepowermgrsample1.databinding.ActivityOsupdateBinding;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+public class MainActivity extends AppCompatActivity implements MXPowerManagerHelper.EventListener {
+
+    private MXPowerManagerHelper powerManagerHelper = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Assign the profile name used in EMDKConfig.xml
+        String profileName = "OSUpdateProfile";
+        powerManagerHelper = new MXPowerManagerHelper(this);
+        powerManagerHelper.setupEMDK(this, profileName);
+
+        ActivityOsupdateBinding binding = ActivityOsupdateBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // References of the UI elements
+        statusTextView = (TextView) findViewById(R.id.textViewStatus);
+        pwrRadioGroup = (RadioGroup) findViewById(R.id.radioGroupPwr);
+        zipFilePathEditText = (EditText) findViewById(R.id.et_zip_file_path);
+
+        // Set on Click listener to the set button to execute Power Manager
+        // operations
+        addSetButtonListener();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        //Clean up the objects created by EMDK manager
+        powerManagerHelper.releaseEMDK();
+    }
+
+    // Text View for displaying status of EMDK operations
+    private TextView statusTextView = null;
+
+    // Radio Group to hold Radio Buttons for Power Manager Options
+    private RadioGroup pwrRadioGroup = null;
+
+    // Edit Text that allows user to enter the path of the update package from
+    // external SD Card
+    private EditText zipFilePathEditText;
+
+    // Method to set on click listener on the Set Button
+    private void addSetButtonListener() {
+
+        // Get Reference to the Set Button
+        Button setButton = (Button) findViewById(R.id.buttonSet);
+
+        // On Click Listener
+        setButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Get Reference to the Radio Buttons that show various Power
+                // Manager Options
+                int radioid = pwrRadioGroup.getCheckedRadioButtonId();
+
+                if (radioid == R.id.radioSuspend)
+                    powerManagerHelper.callFeature(MXPowerManagerHelper.Options.SLEEP_MODE, null);
+
+                if (radioid == R.id.radioReset)
+                    powerManagerHelper.callFeature(MXPowerManagerHelper.Options.REBOOT, null);
+
+                if (radioid == R.id.radioOSUpdate)
+                    powerManagerHelper.callFeature(MXPowerManagerHelper.Options.OS_UPDATE, zipFilePathEditText.getText().toString());
+
+            }
+        });
+    }
+
+    @Override
+    public void onEMDKSessionOpened() {
+        statusTextView.setText("EMDK open success.");
+    }
+
+    @Override
+    public void onEMDKSessionClosed() {
+
+    }
+
+    @Override
+    public void onEMDKError(MXPowerManagerHelper.XMLErrorInfo errorInfo) {
+        if (errorInfo.errorType == "File Path") {
+            Toast.makeText(this.getApplicationContext(), errorInfo.errorDescription,
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            // Show dialog of Failure
+            AlertDialog.Builder builder = new AlertDialog.Builder(this.getApplicationContext());
+            builder.setTitle(errorInfo.errorName);
+            builder.setMessage(errorInfo.errorDescription).setPositiveButton(
+                    "OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    }
+}
